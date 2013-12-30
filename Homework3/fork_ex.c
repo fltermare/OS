e0a3e1cb5c8371da6c72e196bfe0ebc63dbd9b33
Albert Su <fltermare@gmail.com>
2013-12-31 00:43:03 +0800

    OS3 finished

--- Homework3/fork_ex.c
+++ Homework3/fork_ex.c
@@ -11,25 +11,23 @@ void ChildProcess()
 {
     int temp;
     sleep(2);
-    printf("Child pid: %d.  [Var 'mem_alloc']vaddr: 0x%lx, val: %d\n", getpid(), mem_alloc, *mem_alloc);
+    printf("Child pid: %d.  [Var 'mem_alloc']vaddr: 0x%lx, paddr: 0x%lx, val: %d\n", getpid(), mem_alloc, syscall(314, getpid(), mem_alloc), *mem_alloc);
     
     temp = *mem_alloc;
     *mem_alloc = 1;
     printf("\n*** Modify variable 'mem_alloc' from %d to %d ***\n\n", temp, *mem_alloc);
 
     sleep(2);
-    printf("Child pid: %d.  [Var 'mem_alloc']vaddr: 0x%lx, val: %d\n", getpid(), mem_alloc, *mem_alloc);
+    printf("Child pid: %d.  [Var 'mem_alloc']vaddr: 0x%lx, paddr: 0x%lx, val: %d\n", getpid(), mem_alloc, syscall(314, getpid(), mem_alloc), *mem_alloc);
 
 }
 
 void ParentProcess()
 {
     sleep(1);
-    printf("Parent pid: %d. [Var 'mem_alloc']vaddr: 0x%lx, val: %d\n", getpid(), mem_alloc, *mem_alloc);
-    
+    printf("Parent pid: %d. [Var 'mem_alloc']vaddr: 0x%lx, paddr: 0x%lx, val: %d\n", getpid(), mem_alloc, syscall(314, getpid(), mem_alloc), *mem_alloc);
     sleep(3); 
-    printf("Parent pid: %d. [Var 'mem_alloc']vaddr: 0x%lx, val: %d\n", getpid(), mem_alloc, *mem_alloc);
-
+    printf("Parent pid: %d. [Var 'mem_alloc']vaddr: 0x%lx, paddr: 0x%lx, val: %d\n", getpid(), mem_alloc, syscall(314, getpid(), mem_alloc), *mem_alloc);
 }
 
 
--- Homework3/lookup_paddr.c
+++ Homework3/lookup_paddr.c
@@ -48,7 +48,16 @@ unsigned long virtaddr_to_physaddr(struct mm_struct *mm, unsigned long vaddr)
     else{
         /*Need to implement*/
         /*...................*/
-        /*...................*/
+		pte = pte_offset(pud, vaddr);
+        printk("pte_val = 0x%lx\n", pte_val(*pte));
+	    printk("pte_index = %lx\n", pte_index(vaddr));
+	    if(pte_none(*pte)){
+			printk("not mapped in pte\n");
+			return INVALID_ADDR;
+		}
+		paddr = (pte_val(*pte) & PAGE_MASK);
+		paddr = paddr | (vaddr & ~PAGE_MASK);
+		/*...................*/
         /*End of implement*/
         return paddr;
     }
